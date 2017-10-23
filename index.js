6360ea65fdd45a800bafa53e7c6b5ec223e2c7c7
John Peterson <vengefulvidarr@gmail.com>
2017-10-23 12:26:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
-var toUpperCase = 'HELLO!'
-
-var toLowerCase = 'hello!'
-
-var mixedCase = 'Hi there!'
-
 function shout(string) {
   console.log (string);
   return string.toUpperCase()
 }
 
+
+
 function whisper(string) {
   console.log(string);
   return string.toLowerCase()
-}
-
-function logShout(string){
-  console.log(string)
-  const spy = expect.spyOn(console, 'log').andCallThrough()
-}
-
-function logWhisper(string) {
-  console.log(string);
-}
-
-function toUpperCase(string) {
-
-}
-
-function sayHiToGrandma(string) {
-  if (`string` === toUpperCase) {
-    return "I can\'t hear you!"
-  }
-  else if (`string` === toLowerCase){
-    return "YES INDEED!"
-  }
-  else if (`string` === "I love you, Grandma.") {
-    return "I love you too."
-  }
 }
