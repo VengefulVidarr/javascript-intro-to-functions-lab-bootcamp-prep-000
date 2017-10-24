5ceaad551ecaa17bfca56781c970900595c6daf4
John Peterson <vengefulvidarr@gmail.com>
2017-10-24 11:07:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string){
   if (`$(string)` === string.toLowerCase) {
-    console.log("I can\'t hear you!");
+    return console.log("I can\'t hear you!");
   }
   else if (`$(string)` === string.toUpperCase){
-    console.log("YES INDEED!");
+    return console.log("YES INDEED!");
   }
 }
