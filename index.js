5250bb852cd338891bd79ec9a47b193045a3223c
John Peterson <vengefulvidarr@gmail.com>
2017-10-24 12:52:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase) {
+  if (string === 'hello') {
     return "I can\'t hear you!"}
+  }
+  else if (string === 'HELLO') {
+    return "YES INDEED!"
+  }
+  else if (string === 'I love you, Grandma.') {
+    return "I love you, too."
   }
