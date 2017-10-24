e4f825f0a8723cb1662a1fb82d131f5da70c957b
John Peterson <vengefulvidarr@gmail.com>
2017-10-24 11:46:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-//Yells
 function shout(string) {
-  console.log (string).toUpperCase;
-
-}
-
-//Needs Spy Added
-function logShout(string) {
   console.log(string).toUpperCase;
-  function spy(string){
-    if (`$string` === string.toUpperCase)
-    spyon(console, `log`).andCallThrough
-  }
-}
-
-
-//whisper
-function whisper(string) {
-  console.log(string);
-  return string.toLowerCase();
-}
-
-function logWhisper(string) {
-  console.log(string);
-}
-
-function sayHiToGrandma(string) {
-  if (string.toUpperCase) {
-    return console.log("YES INDEED!");
-  }
 }
